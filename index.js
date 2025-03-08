c21782e0a641d3cd81b855a152f9c7d4f781424a
ProCat41 <klasniykot41@gmail.com>
2025-03-08 12:15:16 +0200

    update 08.03.2025

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const path = require('path')
 const fs = require('fs')
+const url = require('url')
 
 
 const dataPath = path.join(__dirname, 'data')
@@ -8,6 +9,12 @@ const dataPath = path.join(__dirname, 'data')
 const server = http.createServer((req, res) => {
     if(req.url == "/jokes" && req.method == "GET"){
         getAllJokes(req,res)
+    }
+    if(req.url == "/jokes" && req.method == "POST"){
+        addJoke(req,res)
+    }
+    if(req.url.startsWith('/like')){
+        like(res,req)
     }
 })
 
@@ -25,4 +32,57 @@ function getAllJokes(req,res){
         allJokes.push(joke)
     }
     res.end(JSON.stringify(allJokes))
+}
+function addJoke(req, res){
+    let data = ''
+req.on('data', function(chunk){
+    data += chunk
+})
+req.on('end', function(){
+    let joke = JSON.parse()
+    joke.likes = 0
+    joke.disliakes = 0
+
+    let dir = fs.readFileSync(dataPath)
+    let fileName = dir.length+'.json'
+
+    let filePath = path.join(dataPath, filePath)
+    fs.writeFileSync(filePath, json.stringify(joke))
+    res.end()
+    })
+}
+
+function like(req, res){
+    const params = url.parse(req.url, true).query
+    let id = params.id
+    console.log(id)
+    if(id){
+        let filePath = path.join(dataPath, id+'.json')
+        let file = fs.readFileSync(filePath)
+
+        let jokeJSON = Buffer.from(file).toString()
+        let joke = JSON.parse(jokeJSON)
+
+        joke.likes++
+
+        fs.writeFileSync(filePath, JSON.stringify(joke))
+    }
+    res.end()
+}
+function dislike(req, res){
+    const params = url.parse(req.url, true).query
+    let id = params.id
+    console.log(id)
+    if(id){
+        let filePath = path.join(dataPath, id+'.json')
+        let file = fs.readFileSync(filePath)
+
+        let jokeJSON = Buffer.from(file).toString()
+        let joke = JSON.parse(jokeJSON)
+
+        joke.dislikes++
+
+        fs.writeFileSync(filePath, JSON.stringify(joke))
+    }
+    res.end()
 }
